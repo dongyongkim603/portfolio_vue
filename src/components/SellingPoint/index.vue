0fd91c11f09e8600e11806d6b2277079094951cc
John <dongyongkim8@gmail.com>
2023-08-31 12:13:03 -0400

    called selling points from page and created initial component

---
+++ src/components/SellingPoint/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="selling-point">
+    <div class="column">
+      <div class="column is-one-third">
+        <img :src="sellingPoint.main_image.asset.url" alt="Main Image" class="image" />
+      </div>
+      <div class="column">
+        <h2 class="title">{{ sellingPoint.headline }}</h2>
+        <p class="subtitle">{{ sellingPoint.content }}</p>
+        <a :href="sellingPoint.button_link" class="button is-primary">{{ sellingPoint.button }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SellingPoint',
+  props: {
+    sellingPoint: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.column
+.selling-point {
+  padding: 20px;
+  border: 1px solid rgb(36, 129, 206);
+  border-radius: 5px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+}
+
+.image {
+  max-width: 100%;
+}
+
+.title {
+  font-size: 1.5rem;
+  margin-bottom: 10px;
+}
+
+.subtitle {
+  margin-bottom: 20px;
+}
+
+.button {
+  padding: 10px 20px;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{headline}}</h1>
     <Banner 
       :imageUrl="banner"
     />
     <ImageGalary
+      v-if="imageData.length > 0"
       :images="imageData"
     />
+    <div class="columns">
+      <SellingPoint
+        v-for="sellingPoint in sellingPoints"
+        :key="sellingPoint.id"
+        :sellingPoint="sellingPoint"
+      />
+    </div>
   </div>
 </template>
 
@@ -14,20 +22,24 @@
 import fetchSanity from '../helpers/sanity'
 import ImageGalary from './../components/ImageGallary/index.vue'
 import Banner from './../components/Banner/index.vue'
+import SellingPoint from './../components/SellingPoint/index.vue'
 
 export default {
   name: 'About',
   components: {
     ImageGalary,
-    Banner
+    Banner,
+    SellingPoint
   },
   data() {
     return {
       banner: '',
+      headline: '',
       imageData: [
-        { id: 1, url: 'path_to_your_image1.jpg' },
-        { id: 2, url: 'path_to_your_image2.jpg' },
-      ]
+        // { id: 1, url: 'path_to_your_image1.jpg' },
+        // { id: 2, url: 'path_to_your_image2.jpg' },
+      ],
+      sellingPoints: []
     }
   },
   async beforeCreate() {
@@ -38,6 +50,19 @@ export default {
         url,
         name,
         headline,
+        components[]{
+          button,
+          button_link,
+          headline,
+          content,
+          main_image{
+            asset->{
+              _id,
+              url
+            },
+            alt
+          }
+        },
         banner{
           asset->{
             _id,
@@ -46,8 +71,9 @@ export default {
           alt
         }
       }`)
-    this.banner = pageData[0].banner.asset.url
-    console.log(this.banner)
+    this.banner = pageData[0]?.banner?.asset?.url
+    this.headline = pageData[0]?.headline
+    this.sellingPoints = pageData[0]?.components
   },
 }
 </script>
