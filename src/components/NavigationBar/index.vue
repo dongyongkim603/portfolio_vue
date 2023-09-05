ca6c135b8da1b517003ed0cd04b41930e9527fcf
John <dongyongkim8@gmail.com>
2023-09-05 12:51:41 -0400

    added a dynamic login/user icon

--- src/components/NavigationBar/index.vue
+++ src/components/NavigationBar/index.vue
@@ -24,8 +24,27 @@
         <router-link to="/gallery" class="navbar-item">gallery</router-link>
         <router-link to="/forums" class="navbar-item">forum</router-link>
         <div class="navbar-item">
-          <div class="buttons">
+          <div v-if="!$store.state.token" class="buttons">
             <router-link to="/log-in" class="button is-light">Log in</router-link>
+          </div>
+          <div v-else class="buttons">
+            <div class="user-dropdown">
+              <!-- User Image or Icon -->
+              <button @click="toggleDropdown">
+                <i v-if="!hasUserImage" class="fas fa-user"></i>
+                <img v-else :src="userImageUrl" alt="User" />
+              </button>
+
+              <!-- Dropdown Menu -->
+              <div v-if="showDropdown" class="dropdown-menu">
+                <!-- Dropdown content goes here -->
+                <ul>
+                  <li>Profile</li>
+                  <li>Settings</li>
+                  <li>Logout</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -37,12 +56,49 @@
 export default {
   data() {
     return {
-      showMobileMenu: false
+      showMobileMenu: false,
+      hasUserImage: false, // Set this to true if user image is present
+      showDropdown: false
+    }
+  },
+  computed: {
+    userImageUrl() {
+      // Return the user image URL here if available
+      return '/path/to/user/image.jpg';
+    }
+  },
+  methods: {
+    toggleDropdown() {
+      this.showDropdown = !this.showDropdown;
     }
   }
 }
 </script>
-  
-  <style>
-  
-  </style>
+
+<style scoped>
+.user-dropdown {
+  position: relative;
+}
+
+.dropdown-menu {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  display: none;
+  z-index: 10;
+  background-color: yellowgreen;
+  border-radius: 5px;
+  padding: 0.5rem;
+}
+
+.user-icon {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.user-dropdown button:focus + .dropdown-menu {
+  display: block;
+}
+</style>
