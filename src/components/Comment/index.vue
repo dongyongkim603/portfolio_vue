ca33a8069ea6be6525a62dbdfcdc69f0a5e17d13
John <dongyongkim8@gmail.com>
2023-09-08 13:33:01 -0400

    added user thumbnail to comment and fixed style

--- src/components/Comment/index.vue
+++ src/components/Comment/index.vue
@@ -3,13 +3,13 @@
     <article class="media">
       <div class="media-left">
         <figure class="image is-48x48">
-          <img :src="comment.get_image" alt="User Avatar">
+          <img :src="comment.get_creator_thumbnail" :alt="`${comment.get_creator}'s User Avatar`">
         </figure>
       </div>
       <div class="media-content">
-        <div class="content">
+        <div class="comment-content">
           <p>
-            <strong>{{ comment.get_creator }}</strong>
+            <strong>@{{ comment.get_creator }}</strong> | {{comment.date_added}}
             <br>
             {{ comment.content }}
           </p>
@@ -28,5 +28,7 @@ export default {
 </script>
 
 <style scoped>
-/* Add your custom CSS styles here */
+  .comment-content {
+    margin: auto 2rem auto auto 2rem;
+  }
 </style>
